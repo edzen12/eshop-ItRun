4f7ff2f81038dc0cceabc0ecc569836685e5d270
edzen12 <oichiev.edzen@gmail.com>
2025-01-16 19:43:50 +0600

    done category, install mptt, spliting the main template

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.urls import reverse
+from mptt.models import MPTTModel
+from mptt.fields import TreeForeignKey
 
-# Create your models here.
+
+class Category(MPTTModel):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    parent = TreeForeignKey(
+        'self', blank=True, null=True, 
+        related_name='children', on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=50, verbose_name="Название категории")
+    keywords = models.CharField(max_length=255, verbose_name="ключевые слова для категории")
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField(null=False, unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("category_detail", kwargs={"slug": self.slug})
+    
