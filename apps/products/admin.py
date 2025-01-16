095051a16262314a00968c7772c927f383ef6545
edzen12 <oichiev.edzen@gmail.com>
2025-01-16 20:14:07 +0600

    done product model, setting admin.py

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.products.models import Category, Product
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product)
--- apps/products/models.py
+++ apps/products/models.py
@@ -16,7 +16,7 @@ class Category(MPTTModel):
     title = models.CharField(max_length=50, verbose_name="Название категории")
     keywords = models.CharField(max_length=255, verbose_name="ключевые слова для категории")
     image = models.ImageField(blank=True, upload_to='images/')
-    status = models.CharField(max_length=10, choices=STATUS)
+    status = models.CharField(max_length=10, choices=STATUS,default='True')
     slug = models.SlugField(null=False, unique=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
@@ -25,4 +25,32 @@ class Category(MPTTModel):
 
     def get_absolute_url(self):
         return reverse("category_detail", kwargs={"slug": self.slug})
+
+    class Meta:
+        verbose_name_plural = 'Категории'
+        verbose_name = 'категория'
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'True'),
+        ('False', 'False'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, verbose_name="Название")
+    keywords = models.CharField(max_length=255, verbose_name="ключевые слова для товара")
+    image = models.ImageField(blank=True, upload_to='images/')
+    description = models.TextField(verbose_name="Описание")
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField(null=False, unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"slug": self.slug})
     
+    class Meta:
+        verbose_name_plural = 'Товары'
+        verbose_name = 'товар'
