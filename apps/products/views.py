ca2269a6415717ad49e5470c9f2a70eec994b551
edzen12 <oichiev.edzen@gmail.com>
2025-01-17 20:04:21 +0600

    вывод категории в сайдбар

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from apps.products.models import Product
+from apps.products.models import Product, Category
 
 def home(request):
     products = Product.objects.all()
-    context = {'products':products}
+    sliders = Product.objects.all()[:2]
+    categories = Category.objects.all()[:8]
+    context = {
+        'products':products,
+        'sliders':sliders,
+        'categories':categories,
+    }
     return render(request, 'index.html', context)
