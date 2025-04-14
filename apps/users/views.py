e1bae05c275147055b9ad5e487a2505102c5b296
Эдзэн Ойчиев <edzenojciev@MacBook-Air-Edzen.local>
2025-04-14 18:23:12 +0600

    edit html

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login as user_login, logout as user_logout, get_user_model
 from django.contrib.auth.decorators import login_required
 from apps.products.models import Category
+User = get_user_model()
 
 
 def login_view(request):
@@ -17,3 +18,35 @@ def login_view(request):
         else:
             return render(request, 'auth/login.html', {'error':'Неверный логин или пароль', 'categories':categories})
     return render(request, 'auth/login.html', {'categories':categories})
+
+
+def reg_view(request):
+    categories = Category.objects.all()[:6]
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password != password2:
+            return render(request, 'auth/register.html', {'error':'Пароли не совпадают', 'categories':categories})
+        if len(password)< 6:
+            return render(request, 'auth/register.html', {'error':'Пароль должен быть больше 6 символов', 'categories':categories})
+        if password == password2:
+            User.objects.create_user(username=login, password=password)
+            usr = authenticate(request, username=login, password=password)
+            if usr is not None: # проверка на сущ
+                user_login(request, usr)
+                return HttpResponseRedirect('/')
+            else:
+                return render(request, 'auth/register.html', {'error':'Неверный логин или пароль', 'categories':categories})
+    return render(request, 'auth/register.html', {'categories':categories})
+
+
+def logout_view(request):
+    user_logout(request)
+    return HttpResponseRedirect('/')
+
+
+@login_required
+def profile_view(request):
+    categories = Category.objects.all()[:6]
+    return render(request, 'auth/profile.html', {'categories':categories})
--- eshop/urls.py
+++ eshop/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from apps.products.views import home, product_detail, category_detail, contact
-from apps.users.views import login_view
+from apps.users.views import login_view, reg_view, logout_view, profile_view
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,7 +9,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('contact/', contact, name='contact'),
+    path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
+    path('register/', reg_view, name='register'),
+    path('profile/', profile_view, name='profile'),
     path('product/<slug:slug>/', product_detail, name='product_detail'),
     path('category/<slug:slug>/', category_detail, name='category_detail')
 ]
