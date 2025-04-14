1604b7dd02ba012fae99cdea9b4eed93c3d086ce
Эдзэн Ойчиев <edzenojciev@MacBook-Air-Edzen.local>
2025-04-14 19:36:43 +0600

    edit language

--- eshop/settings.py
+++ eshop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -114,6 +116,13 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+
+gettext = lambda s: s
+LANGUAGES = (
+    ('ru', gettext('Russian')),
+    ('en', gettext('English')),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- eshop/urls.py
+++ eshop/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 from apps.products.views import home, product_detail, category_detail, contact
 from apps.users.views import login_view, reg_view, logout_view, profile_view
 from django.conf import settings
@@ -7,14 +8,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('contact/', contact, name='contact'),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
     path('register/', reg_view, name='register'),
+]
+urlpatterns += i18n_patterns(
+    path('', home, name='home'),
+    path('contact/', contact, name='contact'),
     path('profile/', profile_view, name='profile'),
     path('product/<slug:slug>/', product_detail, name='product_detail'),
     path('category/<slug:slug>/', category_detail, name='category_detail')
-]
+)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
